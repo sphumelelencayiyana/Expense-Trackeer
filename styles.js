731b5c29d845efff912576e1039c6a28fa1f5318
e <sphumelele762>
2022-07-05 14:01:45 +0200

    add fix js

---
+++ styles.js
@@ -0,0 +1,9 @@
+var state = {
+    balance:1000 ,
+    income: 400,
+    expense: 100 ,
+    transaction: [
+        { name:'Salary', amount: 1000, type: 'income'},
+        { name:' Buy Grocery', amount: 50, type: 'expense'},
+        { name:'Buy Guitar ', amount: 500, type: 'expense'},
+    ]
